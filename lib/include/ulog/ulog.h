7076332291e1d4d40d74392956bf0ca045523641
anomi <git.dimi@mailbox.org>
2022-02-25 22:20:06 +0100

    refactor ulog.h

--- lib/include/ulog/ulog.h
+++ lib/include/ulog/ulog.h
@@ -3,16 +3,17 @@
 
 #include <stdint.h>
 #include <stddef.h>
-#include <assert.h>
 #include "cpp_magic.h"
 #include "ulog/ulog_arg.h"
 #include "ulog/ulog_hash.h"
+#include "ulog/internal/toolchain.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #define ULOG_SECTION_RODATA ".ulog_rodata"
+#define __ulog_rodata __section(ULOG_SECTION_RODATA)
 
 typedef int (*ulog_backend_handle_init_t)(void* arg);
 typedef int (*ulog_backend_handle_tx_t)(uint8_t const* data, size_t size);
@@ -31,11 +32,16 @@ typedef struct
   size_t size;
 } ulog_config_t;
 
+/**
+ * @brief printf like function to force the compiler to check the format string
+ */
+static inline __printf_like(1, 2) void ulog_dummy_printf(char const* fmt, ...) { (void)fmt; }
+
 int ulog_init(ulog_config_t const* config, ulog_backend_t const* backend, void* backend_arg);
 int ulog_vprintf(const uint32_t tag, ulog_arg_t const* arg_list, size_t size);
 void ulog_deinit(void);
 
-#define ARG_LIST_ENTRY(x) {ulog_arg_type_id(x), (ulog_arg_value_t)x}
+#define ARG_LIST_ENTRY(x) (ulog_arg_t){ulog_arg_type_id(x), (ulog_arg_value_t)x}
 #define MAP_ARGS(op,sep,...) \
   IF(HAS_ARGS(__VA_ARGS__))(EVAL(MAP_ARGS_INNER(op,sep,__VA_ARGS__)))
 #define MAP_ARGS_INNER(op,sep,cur_val_1, ...) \
@@ -45,18 +51,16 @@ void ulog_deinit(void);
   )
 #define _MAP_ARGS_INNER() MAP_ARGS_INNER
 
-#define ULOG_RAW(_fmt, ...) \
-do \
-{ \
-    static const char fmt[] __attribute__((section(ULOG_SECTION_RODATA))) = _fmt; \
-    (void)fmt; \
+#define ULOG_RAW(_fmt, ...) do { \
+    static const char fmt[] __ulog_rodata = _fmt; \
+    ULOG_HASH_STR_ASSERT(_fmt); \
     static const uint32_t tag = ULOG_HASH(_fmt); \
-    static_assert(tag == 0, "format string is too long"); \
     IF(HAS_ARGS(__VA_ARGS__))( \
+        ulog_dummy_printf(fmt, __VA_ARGS__); \
         const ulog_arg_t arg_list[] = {MAP_ARGS(ARG_LIST_ENTRY, COMMA, __VA_ARGS__)}; \
         ulog_vprintf(tag, arg_list, sizeof(arg_list) / sizeof(arg_list[0])); \
     ) \
-    IF(NOT(HAS_ARGS(__VA_ARGS__)))(ulog_vprintf(tag, NULL, 0);) \
+    IF(NOT(HAS_ARGS(__VA_ARGS__)))((void)fmt; ulog_vprintf(tag, NULL, 0);) \
 } while (0)
 
 #define ULOG_DBG(_fmt, ...) ULOG_RAW("DBG: " _fmt "\n", ##__VA_ARGS__)
